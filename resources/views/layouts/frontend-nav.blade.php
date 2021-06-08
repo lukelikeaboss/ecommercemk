6056770cbfb924115bb362e8c22ddee1785609b6
lukelikeaboss <mlkimani2@gmail.com>
2021-06-08 17:33:24 +0300

    begining with welcome.blade adding a header and a footer

---
+++ resources/views/layouts/frontend-nav.blade.php
@@ -0,0 +1,21 @@
+<div class="nav-bar">
+    <nav class="navbar navbar-expand-md bg-red navbar-green">
+
+        <a href="#" class="navbar-brand"></a> <img src="#" class="img-fluid logo-image" alt="Brand logo"></a>
+        <button type="button" data-target="#main-navigation"
+                class="navbar-toggle navbar-dark" data-toggle="collapse">
+            <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navigation">
+            <ul class="navbar-nav ml-auto navbar-dark ">
+                <li  class="nav-item"> <a class="nav-link" href="#">Home</a></li>
+                <li  class="nav-item"> <a class="nav-link" href="#">About</a></li>
+                <li  class="nav-item"> <a class="nav-link" href="#">Contacts</a></li>
+                <li  class="nav-item"> <a class="nav-link" href="#">Shop</a></li>
+                <li  class="nav-item"> <a class="nav-link" href="#">Login</a></li>
+                <li  class="nav-item"> <a class="nav-link" href="#">Register</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</div>
